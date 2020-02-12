76e20564e58f8e2154ea40d034c3a85ec436cbd1
johnrrowe <jrrowe@clemson.edu>
2020-02-12 16:39:31 -0500

    removed sensitive info

---
+++ db_connector.py
@@ -0,0 +1,28 @@
+import requests
+import mysql.connector
+from mysql.connector import errorcode
+from mysql.connector.constants import ClientFlag
+
+config = {
+  'host':'osrs.mysql.database.azure.com',
+  'user':'jrrowe@osrs',
+  'password':
+  'database':'items'
+}
+
+def connect_to_db():
+    # Try to connect to database
+    try:
+       conn = mysql.connector.connect(**config, client_flags=[ClientFlag.LOCAL_FILES])
+       print("Connection established.")
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+          print("Something is wrong with the user name or password")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+          print("Database does not exist")
+        else:
+            print(err)
+    else:
+        cursor = conn.cursor()
+
+    return conn, cursor
